484c100b0a5759c7e7fee5099af06fd54a0b218e
tomaszdworniczak <tomasz.dworniczak@start-up.house>
2021-08-19 11:27:13 +0200

    Add cors middleware to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
-import { UsersModule } from './users/users.module';
+import {MiddlewareConsumer, Module, NestModule, RequestMethod} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ProductsModule} from './products/products.module';
+import {UsersModule} from './users/users.module';
+import * as cors from 'cors';
 
 @Module({
   imports: [ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer
+        .apply(cors())
+        .forRoutes({
+          path: '*',
+          method: RequestMethod.ALL
+        })
+  }
+}
